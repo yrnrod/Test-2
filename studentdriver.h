46480d7b6cde45ca265d1ad2a35f103139e3fe4a
Darryl Brooks <darryl1.brooks@famu.edu>
2019-04-20 23:04:52 -0400

    Update studentdriver.h

--- studentdriver.h
+++ studentdriver.h
@@ -3,18 +3,20 @@
 #include <iomanip>
 using namespace std;
 
-double Student::calcSemGrade()
+double Student::calcSemGrade()                                         //This should be in the the other cpp file 
 {
-    cout << calcSemGrade << endl;
+    cout << calcSemGrade << endl;           
 }
 
 
 int main()
 {
- double grade, midtermg, finalg;
+    double grade, midtermg, finalg;                                  // Student studentObject;
+                                                                     // studentObject.grade is the samething as defining them in main so it's no need to make new variables 
     
-cout << "What are your last two semester grades?" << endl;
-    cin >> midtermg;
-    cin >> finalg;
-   
+    cout << "What are your last two semester grades?" << endl;       //When you overload the operators you can have all of this inside it 
+    cin >> midtermg;                                                 //After you overload the operators you can simply do cout<<studentObject and it'll print however you created the overloader to do
+    cin >> finalg;  
+    
+   return 0;
 }
