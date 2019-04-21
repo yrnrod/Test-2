2b093d26cc5e6d35a0c4e13f605aba7014f3f032
yrnrod <48459801+yrnrod@users.noreply.github.com>
2019-04-20 21:20:44 -0400

    Create studentdriver.h

---
+++ studentdriver.h
@@ -0,0 +1,20 @@
+#include "student.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double Student::calcSemGrade()
+{
+    cout << calcSemGrade << endl;
+}
+
+
+int main()
+{
+ double grade, midtermg, finalg;
+    
+cout << "What are your last two semester grades?" << endl;
+    cin >> midtermg;
+    cin >> finalg;
+   
+}
