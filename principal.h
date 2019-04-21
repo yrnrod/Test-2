825fb5d3da2e46de2bf776453880a18b3112457b
yrnrod <48459801+yrnrod@users.noreply.github.com>
2019-04-20 21:21:24 -0400

    Create principal.h

---
+++ principal.h
@@ -0,0 +1,18 @@
+#ifndef PRINCIPAL_H
+#define PRINCIPAL_H
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+class Principal
+{
+    private:
+    long emergencyContact;
+    int studentGradelevel;
+    
+    public:
+    
+}
